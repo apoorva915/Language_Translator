b5e94c3ebd70532d8822f59072701569d466c3d2
apoorva915 <124496771+apoorva915@users.noreply.github.com>
2024-03-13 20:28:22 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+import PyPDF2
+
+def extract_text_from_pdf(pdf_file):
+    with open(pdf_file,'rb') as pdf:
+      reader = PyPDF2.PdfReader(pdf, strict = False)
+      pdf_text = []
+
+      for page in reader.pages:
+        content=page.extract_text()
+        pdf_text.append(content)
+
+    return pdf_text
+    
+if __name__=='__main__':
+    extracted_text=extract_text_from_pdf('Lec-3.pdf')
+    print(extracted_text)
